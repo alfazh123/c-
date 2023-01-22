3007c96e9e089dd7a387445b8ecb92d4bc3a98ce
Ahmd Mufahras Li Alfazh Assardew <117156042+alfazh123@users.noreply.github.com>
2023-01-22 20:52:12 +0700

    Add files via upload

---
+++ tugas awal/keliling_lingkaran.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std ;
+//NAMA :AHMD MUFAHRAS LI ALFAZH ASSARDEW
+//NIM  :22051204020
+//KELAS:TIA2022
+
+
+int main(int argc, char const *argv[])
+{
+
+    float d,p= 3.14159 ;
+    // r=jari-jari
+    cout << "masukkan nilai d (diameter)= " ;
+    cin >> d ;
+    cout << "keliling lingkaran = " ;
+    cout << d*p ;
+    return 0;
+}
---
+++ tugas awal/penjumlahan_2_variabel.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std ;
+//NAMA :AHMD MUFAHRAS LI ALFAZH ASSARDEW
+//NIM  :22051204020
+//KELAS:TIA2022
+
+
+int main(int argc, char const *argv[])
+{
+    int a,b;
+
+    cout << " masukkan nilai a = " ;
+    cin >> a ;
+    cout << " masukkan nilai b = " ;
+    cin >> b ;
+    cout << a+b <<endl;
+    return 0;
+}
